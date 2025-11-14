3cdc8135e55686254cd349ea991a2ee6078219d2
saffirebandril <safban917@gmail.com>
2025-11-14 14:47:05 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a,b):
+    return math.hypot(a,b)
+
 def add(a, b):
     return a + b
 
