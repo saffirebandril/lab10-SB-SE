7a18b7bb4004f0a772b2b3d92eb49fdf214bf2cf
saffirebandril <safban917@gmail.com>
2025-11-16 21:44:15 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,17 +4,17 @@
 
 import unittest
 from calculator import *
-import calculator
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(calculator.add(1,2),3)
-        self.assertEqual(calculator.add(-5,5),0)
-        self.assertEqual(calculator.add(10,-3),7)
+        self.assertEqual(add(1,2),3)
+        self.assertEqual(add(5, -5),0)
+        self.assertEqual(add(10,-3),7)
+
     def test_sub(self):
-        self.assertEqual(calculator.sub(5,2),3)
-        self.assertEqual(calculator.sub(10,10),0)
-        self.assertEqual(calculator.sub(-3,-6),3)
+        self.assertEqual(subtract(5,2),3)
+        self.assertEqual(subtract(10,10),0)
+        self.assertEqual(subtract(-3,-6),3)
 
     def test_multiply(self):
         self.assertEqual(mul(3, 5), 15)
@@ -28,14 +28,20 @@ class TestCalculator(unittest.TestCase):
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            calculator.divide(10,0)
+            div(0, 10)
+
     def test_logarithm(self):
-        self.assertEqual(calculator.log(8,2),3)
-        self.assertEqual(calculator.log(27,3),3)
-        self.assertEqual(calculator.log(1,10),0)
+        self.assertEqual(logarithm(10,100),2)
+        self.assertEqual(logarithm(2,16),4)
+        self.assertEqual(logarithm(9,3),0.5)
+
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            calculator.logarithm(10,1)
+            logarithm(10,-1)
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
 
     def test_hypotenuse(self):
         self.assertEqual(hypotenuse(3,4),5)
@@ -49,13 +55,6 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(square_root(9),3)
         self.assertNotEqual(square_root(2),2)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
 
 # Do not touch this
 if __name__ == "__main__":
