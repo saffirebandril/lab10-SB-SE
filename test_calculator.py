62926b3bc9183299d2b405bb5af75e677ac8c59c
Sebastian Estrada <sestrada25@belenwolverines.org>
2025-11-16 20:16:33 -0500

    push

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,25 @@
+#https://github.com/saffirebandril/lab10-SB-SE.git
+#Partner 1: Saffire Bandril
+#Partner 2: Sebastian Estrada
+
 import unittest
 from calculator import *
+import calculator
+import math
+
+
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_add(self):
+        self.assertEqual(calculator.add(1, 2), 3)
+        self.assertEqual(calculator.add(-5, 5), 0)
+        self.assertEqual(calculator.add(10, -3), 7)
+
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(5, 2), 3)
+        self.assertEqual(calculator.subtract(10, 10), 0)
+        self.assertEqual(calculator.subtract(-3, -6), 3)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,20 +29,18 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            calculator.divide(10, 0)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertEqual(calculator.logarithm(8, 2), 3)
+        self.assertEqual(calculator.logarithm(27, 3), 3)
+        self.assertEqual(calculator.logarithm(1, 10), 0)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(10, 1)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
